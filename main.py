cf2c9f199ad2ee4de55d3f37b0c420d022b20ca8
rath <153273671+shadowcone@users.noreply.github.com>
2024-02-19 22:40:56 +0530

    Update: interface in main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from sklearn.neighbors import KNeighborsClassifier
 
 # Function to train the model and perform prediction
 def train_and_predict(input_csv_path):
-    # Train the model
+
     data = pd.read_csv('ccd.csv')
 
-    X = data[['Age', 'Salary']]
+    x = data[['Age', 'Salary']]
     y = data['Buy']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
     knn = KNeighborsClassifier(n_neighbors=2)
-    knn.fit(X_train, y_train)
+    knn.fit(x_train, y_train)
 
     # Process the input CSV file
     input_data = pd.read_csv(input_csv_path)
@@ -45,14 +45,15 @@ def perform_prediction():
 
 # Create main window
 root = tk.Tk()
+root.configure(bg="#9290C3")
 root.title("CreditCue")  # Set the title to "CreditCue"
 
 # Create frame for input fields
-input_frame = tk.Frame(root)
+input_frame = tk.Frame(root, bg="#9290C3")
 input_frame.pack(pady=10)
 
 # Add label for CSV file path entry field
-csv_label = tk.Label(input_frame, text="Enter CSV File Path:")
+csv_label = tk.Label(input_frame, text="Enter CSV File Path:",bg="#9290C3", fg="#070F2B", font=("Roboto", 12, "bold"))
 csv_label.grid(row=0, column=0, padx=5, pady=5)
 
 # Add entry field for CSV file path
@@ -60,7 +61,7 @@ csv_entry = tk.Entry(input_frame, width=50)
 csv_entry.grid(row=0, column=1, padx=5, pady=5)
 
 # Add button to perform prediction
-predict_button = tk.Button(input_frame, text="Predict", command=perform_prediction)
+predict_button = tk.Button(input_frame, text="Predict", command=perform_prediction, font=("Ariel", 10, "bold"), bg="white", fg="#1B1A55", activebackground="#1B1A55", activeforeground="white")
 predict_button.grid(row=0, column=2, padx=5, pady=5)
 
 # Create frame for output display
