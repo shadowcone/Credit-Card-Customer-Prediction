d3a82b41950c5a4a78a714f3d54b7766c7523237
rath <anniyan@duck.com>
2024-03-14 19:01:06 +0530

    Added: Choose path button

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import filedialog, messagebox
 from tkinter import ttk
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -12,15 +12,16 @@ root.title("CreditCue")
 input_frame = tk.Frame(root, bg="#9290C3")
 input_frame.pack(pady=10)
 
-csv_label = tk.Label(input_frame, text="Enter CSV File Path:",bg="#9290C3", fg="#070F2B", font=("Roboto", 12, "bold"))
-csv_label.grid(row=0, column=0, padx=5, pady=5)
-
 csv_entry = tk.Entry(input_frame, width=50)
 csv_entry.grid(row=0, column=1, padx=5, pady=5)
 
+def select_csv_file():
+    file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+    if file_path:
+        csv_entry.delete(0, tk.END)
+        csv_entry.insert(0, file_path)
 
 def train_and_predict(input_csv_path):
-
     data = pd.read_csv('customers.csv')
 
     x = data[['Age', 'Salary']]
@@ -38,20 +39,20 @@ def train_and_predict(input_csv_path):
 
     return input_data
 
-
 def perform_prediction():
     input_csv_path = csv_entry.get()
     try:
         output_data = train_and_predict(input_csv_path)
         output_text.delete(1.0, tk.END) 
         output_text.insert(tk.END, output_data.to_string(index=False))
-
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
-
 predict_button = tk.Button(input_frame, text="Predict", command=perform_prediction, font=("Ariel", 10, "bold"), bg="white", fg="#1B1A55", activebackground="#1B1A55", activeforeground="white")
 predict_button.grid(row=0, column=2, padx=5, pady=5)
+
+select_button = tk.Button(input_frame, text="Choose File", command=select_csv_file, bg="#9290C3", fg="#070F2B", font=("Roboto", 12, "bold"))
+select_button.grid(row=0, column=0, padx=5, pady=5)
 
 output_frame = tk.Frame(root)
 output_frame.pack(pady=10)
